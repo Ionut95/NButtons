1d325f62b76d0855bdd61d948e499d785107e727
Ionut95 <105048900+Ionut95@users.noreply.github.com>
2022-05-30 19:27:18 +0300

    Update Nbuttons.js

--- Nbuttons.js
+++ Nbuttons.js
@@ -1,17 +1,20 @@
 function generate() {
-	var noBtns = document.getElementById("myNumber").value;
-	for (var index = 1; index <= noBtns; ++index) {
-	  	const button = document.createElement("button");
-	  	button.innerText = "Click me";
-	  	button.value = index;
-	  	button.addEventListener("click", function() {
-	    	var result = Math.floor(Math.random() * noBtns) + 1;
-	    	if (button.value == result) {
-	      		alert('You win!');
-	    	} else {
-	      		alert('You lose!');
-	    	}
-		});
-	  	document.body-myBody.appendChild(button);
-	}
+   var noBtns = document.getElementById("myNumber").value;
+   for (var index = 1; index <= noBtns; ++index) {
+      const button = document.createElement("button");
+      button.innerText = "Click me";
+      button.value = index;
+      button.addEventListener("click", function() {
+         checkBtn(button.value, noBtns);
+      });
+      document.body-myBody.appendChild(button);
+   }
+}
+function checkBtn(currentBtn, nrBtns) {
+   var result = Math.floor(Math.random() * nrBtns) + 1;
+   if (currentBtn == result) {
+      alert("You win!");
+   } else {
+      alert("You lose!");
+   }
 }
